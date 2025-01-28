64e47b87a92e1a9529dcaa9703d1fd8e2fe43607
Joshua Jude Wuebbolt <joshua.wuebbolt@mail.utoronto.ca>
2025-01-28 16:56:56 -0500

    SC-7 - Fixed bug causing gap between the top of the website and navbar. Also deleted accout.tsx which was placed by accident.

--- webapp/src/components/navbar.tsx
+++ webapp/src/components/navbar.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const NavBar: React.FC = () => {
     return (
-        <nav className="flex justify-around items-center p-4 bg-gray-800 text-white">
-            <div className="flex gap-4">
+        <nav className="flex justify-around items-center bg-gray-800 text-white">
+            <div className="flex gap-4 py-2">
                 <a href="#home" className="hover:underline">Home</a>
             </div>
 
-            <div className="flex gap-4">
+            <div className="flex gap-4 py-2">
                 <a href="#donate" className="hover:underline">Donate</a>
             </div>
 
-            <div className="flex gap-4">
+            <div className="flex gap-4 py-2">
                 <a href="#account" className="hover:underline">Account</a>
             </div>
 
@@ -19,15 +19,15 @@ const NavBar: React.FC = () => {
                 Stock Charity
             </div>
 
-            <div className="flex gap-4">
+            <div className="flex gap-4 py-2">
                 <a href="#about" className="hover:underline">About</a>
             </div>
 
-            <div className="flex gap-4">
+            <div className="flex gap-4 py-2">
                 <a href="#join" className="hover:underline">Join as a Charity</a>
             </div>
             
-            <div className="flex gap-4">
+            <div className="flex gap-4 py-2">
                 <a href="#login" className="hover:underline bg-green-500 text-white px-4 py-2 rounded-full">Login</a>
             </div>
         </nav>
