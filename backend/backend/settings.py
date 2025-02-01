259b7345149b7741deccb32d2ba7c1af40d7a7ad
whlpatricia <whlpatricia@users.noreply.github.com>
2025-02-01 17:21:45 -0500

    Removed secrets and expect local to have secrets.json

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import json
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+with open(os.path.dirname(__file__) + "/../../secrets.json") as s:
+    SECRET_KEY = json.load(s)["django"] # TODO: implement secret key in CI
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
