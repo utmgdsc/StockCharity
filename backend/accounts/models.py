5407b9e30c6650e73a7b2f89620717e3bd89262c
fungucide <w0f1@outlook.com>
2025-02-03 16:11:35 -0500

    Override user model to not require username

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,38 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-class Account(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .user_manager import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+        max_length=255,
+        blank=False
+    )
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
