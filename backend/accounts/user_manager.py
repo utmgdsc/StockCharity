23f8ac5d378043a064cc4970a65dcb1d269e3f2f
William <wc.fung@mail.utoronto.ca>
2025-02-05 15:07:04 -0500

    [SC-16] Added account model (#14)
    * Added account model
    
    * Override user model to not require username

---
+++ backend/accounts/user_manager.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        '''Create and save a user with the given email, and
+        password.
+        '''
+        if not email:
+            raise ValueError("No email provided")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        assert(extra_fields.get("is_staff"))
+        assert(extra_fields.get("is_superuser"))
+        return self._create_user(email, password, **extra_fields)
