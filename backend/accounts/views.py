2acebd4f6aea0173dc6e88e37a4c0fdbb7d5eba0
fungucide <w0f1@outlook.com>
2025-02-05 15:13:02 -0500

    Added backend signup functionality

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    # TODO: Add permission class later
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def retrieve(self, request):
+        if request.auth is None:
+            return Response(status=401)
+        return super().retrieve(self, request)
