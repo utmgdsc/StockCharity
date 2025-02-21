f0e9f5513c79bdb398d304e4710c10d45cfab357
William <wc.fung@mail.utoronto.ca>
2025-02-20 23:07:07 -0500

    [SC-10] Create sign up and register (#17)
    * Added backend signup functionality
    
    * Added register
    
    * Fixed style
    
    * Fixed compile issues
    
    * Addressed review comments

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    # TODO: Add permission class later
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def retrieve(self, request):
+        if request.auth is None:
+            return Response(status=401)
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data, status=200)
