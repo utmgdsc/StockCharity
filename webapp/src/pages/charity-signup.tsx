c8dde2b177c87bfccf8259d4d09c5c72547d9c97
taleen20 <taleenabraham@gmail.com>
2025-02-25 01:25:37 -0500

    SC-25 and SC-26 changes
    [SC-10] Account UTs (#23)
    
    * Added unittests for accounts
    
    * Updated UTs for extra user fields

--- webapp/src/pages/charity-signup.tsx
+++ webapp/src/pages/charity-signup.tsx
@@ -1,11 +1,77 @@
 import React from "react";
-import { useState } from "react";
+import { CharityFormData } from "@/util/request";
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import axios from "axios";
+import { parsePhoneNumberFromString } from "libphonenumber-js";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
+const validatePhoneNumber = (phone: string) => {
+
+  if (!phone) return false;
+
+  const formattedPhone = phone.startsWith("+") ? phone : `+${phone}`;
+  const parsed = parsePhoneNumberFromString(formattedPhone);
+  return parsed && parsed.isValid();
+};
+
+const schema = z.object({
+  firstName: z.string().min(1, "First name is required"),
+  lastName: z.string().min(1, "Last name is required"),
+  charityName: z.string().min(1, "Charity name is required"),
+  email: z.string().email("Invalid email address"),
+  phone: z.string().refine(validatePhoneNumber, {
+    message: "Invalid phone number format",
+  }),
+  message: z.string().optional(),
+  isAgreed: z.boolean().refine(val => val === true, "You must accept the privacy policy"),
+});
 
 const CharitySignUp: React.FC = () => {
   const [isAgreed, setIsAgreed] = useState(false);
+  const [userCountry, setUserCountry] = useState("can"); 
+  const [phoneNumber, setPhoneNumber] = useState("+");
+
+  const {
+    register,
+    setValue,
+    trigger,
+    formState: { errors, isValid },
+  } = useForm<CharityFormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      charityName: "",
+      email: "",
+      phone: "",
+      message: "",
+      isAgreed: false,
+    },
+  });
+
+  const handlePhoneChange = (value: string) => {
+    setPhoneNumber(value); 
+    setValue("phone", value); 
+    trigger("phone");
+  };
+  
+  useEffect(() => {
+    axios.get("https://ipapi.co/json/")
+      .then((response) => {
+        setUserCountry(response.data.country_code.toLowerCase());
+      })
+      .catch(() => console.log("Could not fetch user location"));
+  }, []);
 
   const toggleAgreement = () => {
     setIsAgreed(!isAgreed);
+    setValue("isAgreed", !isAgreed); 
+    trigger("isAgreed");
   };
 
   return (
@@ -37,114 +103,88 @@ const CharitySignUp: React.FC = () => {
       <form action="#" method="POST" className="mx-auto w-full max-w-lg sm:mt-0">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label
-              htmlFor="first-name"
-              className="label-style"              >
-              First name
-            </label>
+            <label htmlFor="first-name" className="label-style"> First name</label>   
             <div className="mt-2.5">
               <input
                 type="text"
-                name="first-name"
+                {...register("firstName")}
                 id="first-name"
                 autoComplete="given-name"
                 className="input-style"
               />
+              {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
             </div>
           </div>
+          
           <div>
-            <label
-              htmlFor="last-name"
-              className="label-style"
-              >
-              Last name
-            </label>
+            <label htmlFor="last-name" className="label-style">Last name</label>
             <div className="mt-2.5">
               <input
                 type="text"
-                name="last-name"
+                {...register("lastName")}
                 id="last-name"
                 autoComplete="family-name"
                 className="input-style"
               />
+               {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
             </div>
           </div>
+          
           <div className="sm:col-span-2">
-            <label
-              htmlFor="company"
-              className="label-style"
-              >
-              Charity Name
-            </label>
+            <label htmlFor="charity-name" className="label-style">Charity Name</label>
             <div className="mt-2.5">
               <input
                 type="text"
-                name="company"
-                id="company"
+                {...register("charityName")}
+                id="charity-name"
                 autoComplete="organization"
                 className="input-style"
               />
+              {errors.charityName && <p className="text-red-500">{errors.charityName.message}</p>}
             </div>
           </div>
+          
           <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="label-style"
-              >
-              Email
-            </label>
+            <label htmlFor="email"className="label-style"> Email </label>
             <div className="mt-2.5">
               <input
                 type="email"
-                name="email"
+                {...register("email")}
                 id="email"
                 autoComplete="email"
                 className="input-style"
               />
+              {errors.email && <p className="text-red-500">{errors.email.message}</p>}
             </div>
           </div>
+          
           <div className="sm:col-span-2">
-            <label
-              htmlFor="phone-number"
-              className="label-style"
-              >
-              Phone number
-            </label>
+            <label htmlFor="phone-number" className="label-style">Phone number</label>
             <div className="mt-2.5">
               <div className="flex rounded-md border border-gray-300 bg-white focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2">
-                <select
-                  id="country"
-                  name="country"
-                  autoComplete="country"
-                  aria-label="Country"
-                  className="block w-20 rounded-l-md border-r border-gray-300 bg-white px-3 py-2 text-gray-900 focus:outline-none"
-                >
-                  <option>US</option>
-                  <option>CA</option>
-                  <option>EU</option>
-                </select>
-                <input
-                  type="text"
-                  name="phone-number"
-                  id="phone-number"
-                  className="block w-full rounded-r-md px-3 py-2 text-gray-900 placeholder-gray-400 focus:outline-none"
-                  placeholder="123-456-7890"
+                    <PhoneInput
+                  country={userCountry}
+                  value={phoneNumber}
+                  onChange={handlePhoneChange}
+                  inputStyle={{
+                    width: "100%",
+                    height: "40px",
+                    borderRadius: "5px",
+                    border: "1px solid #ccc",
+                    paddingLeft: "50px",
+                  }}
                 />
               </div>
+              {errors.phone && <p className="text-red-500">{errors.phone.message}</p>}
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="label-style"
-              >
-              Message
-            </label>
+            <label htmlFor="message" className="label-style">Message</label>
             <div className="mt-2.5">
               <textarea
-                name="message"
+                {...register("message")}
                 id="message"
-                rows={4}
+                rows={3}
                 className="input-style"
               ></textarea>
             </div>
@@ -152,8 +192,6 @@ const CharitySignUp: React.FC = () => {
 
           {/* Privacy Agreement Section */}
           <div className="flex items-center gap-x-4 sm:col-span-2">
-
-            {/* Small toggle button */}
             <button
               type="button"
               className={`relative flex w-8 h-4 cursor-pointer rounded-full ${
@@ -171,7 +209,6 @@ const CharitySignUp: React.FC = () => {
               ></span>
             </button>
 
-            {/* Agreement text */}
             <label className="text-sm text-gray-600">
               By selecting this, you agree to our{" "}
               <a href="#" className="font-semibold text-blue-600">
@@ -180,16 +217,21 @@ const CharitySignUp: React.FC = () => {
               .
             </label>
           </div>
-
+          {errors.isAgreed && <p className="text-red-500">{errors.isAgreed.message}</p>}
         </div>
+        
         <div className="mt-10">
           <button
             type="submit"
-            className="btn-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm w-full"
+            disabled={!isValid}
+            className={`btn-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm w-full ${
+              isValid ? "bg-blue-500 hover:bg-blue-600" : "bg-gray-400 cursor-not-allowed"
+            }`}
           >
             Submit
           </button>
         </div>
+        
       </form>
     </div>
   );
