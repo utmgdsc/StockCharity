3572d82e8b638aba3aa926beec4e42018b232c3c
fungucide <w0f1@outlook.com>
2025-03-03 14:52:31 -0500

    Cleanup account sub project

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     first_name = serializers.CharField(required=True)
@@ -12,7 +13,18 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ["email", "first_name", "last_name", "phone_number", "is_staff", "is_active", "password1", "password2", "total_donations", "total_dividends"]
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "is_staff",
+            "is_active",
+            "password1",
+            "password2",
+            "total_donations",
+            "total_dividends",
+        ]
 
     def validate(self, attrs):
         if attrs.get("password1") != attrs.get("password2"):
@@ -20,13 +32,20 @@ class UserSerializer(serializers.ModelSerializer):
 
         phone_number = attrs.get("phone_number", "").lstrip("+")
         if not phone_number.isdigit() or len(phone_number) < 10:
-            raise serializers.ValidationError({"phone_number": "Invalid phone number format"})
+            raise serializers.ValidationError(
+                {"phone_number": "Invalid phone number format"}
+            )
 
         # Update sanitized phone number in attrs
         attrs["phone_number"] = phone_number
 
-        if not attrs.get("phone_number").isdigit() or len(attrs.get("phone_number")) < 10:
-            raise serializers.ValidationError({"phone_number": "Invalid phone number format"})
+        if (
+            not attrs.get("phone_number").isdigit()
+            or len(attrs.get("phone_number")) < 10
+        ):
+            raise serializers.ValidationError(
+                {"phone_number": "Invalid phone number format"}
+            )
 
         return super().validate(attrs)
 
@@ -35,6 +54,8 @@ class UserSerializer(serializers.ModelSerializer):
         password = validated_data.pop("password1")
         phone_number = validated_data.pop("phone_number")
         validated_data.pop("password2")
-        user = get_user_model().objects.create_user(email, phone_number, password, **validated_data)
+        user = get_user_model().objects.create_user(
+            email, phone_number, password, **validated_data
+        )
         user.save()
         return super().to_representation(user)
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,4 +7,14 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("account/", AccountViewSet.as_view({"get": "retrieve"}), name="account"),
+    path(
+        "increase-donations/",
+        AccountViewSet.as_view({"patch": "increase_donations_total"}),
+        name="increase_donation",
+    ),
+    path(
+        "increase-dividends/",
+        AccountViewSet.as_view({"patch": "increase_dividends_total"}),
+        name="increase_dividends",
+    ),
 ]
