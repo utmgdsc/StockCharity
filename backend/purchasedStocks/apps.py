068307c133220058a9c7b45d622cb9d52b5de97d
Hansamu <tintin30052002@gmail.com>
2025-03-10 08:35:40 -0400

    Compute total value per stock as stock_amount * stock_price using annotate() with ExpressionWrapper

--- backend/purchasedStocks/apps.py
+++ backend/purchasedStocks/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StocksConfig(AppConfig):
+class PurchasedStocksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stocks'
+    name = 'purchasedStocks'
