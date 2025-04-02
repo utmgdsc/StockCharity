8d700994836656eee7bb1cef34e85991f1cca508
William <wc.fung@mail.utoronto.ca>
2025-04-02 13:40:51 -0400

    Added model to cache tiingo data (#42)
    Co-authored-by: William <fungucide227@gmail.com>

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -7,3 +7,19 @@ class Stock(models.Model):
 
     def __str__(self):
         return f"{self.stock_name} ({self.symbol}): {self.quantity}"
+
+class StockInfo(models.Model):
+    symbol = models.CharField(max_length=10, primary_key=True)
+    last_update = models.DateField(blank=True, null=True)
+
+class StockInfoEntry(models.Model):
+    stock = models.ForeignKey(StockInfo, on_delete=models.CASCADE, related_name="entries")
+    date = models.DateField()
+    price = models.FloatField()
+    dividend = models.FloatField()
+    split = models.FloatField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["stock", "date"])
+        ]
