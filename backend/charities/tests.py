f5968af1c80644b81b111de637c77e24d5933a00
Joshua Jude Wuebbolt <joshua.wuebbolt@mail.utoronto.ca>
2025-04-02 14:40:02 -0400

    Updated tests to work with updated charities code.

--- backend/charities/tests.py
+++ backend/charities/tests.py
@@ -8,19 +8,33 @@ from .models import Charity
 
 EMAIL = "some@email.com"
 NAME = "Charity"
-PHONE_NO = 123123123
+PHONE_NO = "123123123"
+DESCRIPTION = "A charity organization"
+CONTACT_FIRST_NAME = "John"
+CONTACT_LAST_NAME = "Doe"
+CONTACT_EMAIL = "contact@email.com"
+CONTACT_PHONE_NO = "987654321"
 
 
 class DividendTestCase(APITestCase):
 
     def create_charity(self):
         url = reverse("charity-list")
-        data = {"email": EMAIL, "name": NAME, "phone_number": PHONE_NO}
+        data = {
+            "charity_email": EMAIL,
+            "charity_name": NAME,
+            "charity_phone_number": PHONE_NO,
+            "charity_description": DESCRIPTION,
+            "contact_first_name": CONTACT_FIRST_NAME,
+            "contact_last_name": CONTACT_LAST_NAME,
+            "contact_email": CONTACT_EMAIL,
+            "contact_phone_number": CONTACT_PHONE_NO,
+        }
         return self.client.post(url, data, format="json")
 
-    def update_donation_received(self):
-        url = reverse("charity-increase-donations-received", args=[1])
-        data = {"secret": settings.SECRET_KEY, "donation": 1}
+    def update_donation_received(self, charity_id, donation_amount):
+        url = reverse("charity-increase-donations-received", args=[charity_id])
+        data = {"secret": settings.SECRET_KEY, "donation": donation_amount}
         return self.client.patch(url, data, format="json")
 
     def test_create_charity_ok(self):
@@ -35,11 +49,11 @@ class DividendTestCase(APITestCase):
         self.assertEqual(Charity.objects.count(), 1)
 
     def test_update_donation_received(self):
-        self.create_charity()
-        response = self.update_donation_received()
+        charity_response = self.create_charity()
+        charity_id = charity_response.data["id"] 
+        response = self.update_donation_received(charity_id, 10)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
-            {
-                "donations_received": 1,
-            },
             response.data,
+            {"donations_received": 10},
         )
