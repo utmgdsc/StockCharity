1218e39dda3404ff1ff8bc6c274fe0a9d8881f1a
Hansamu <tintin30052002@gmail.com>
2025-04-09 18:53:10 -0400

    updated init and urls mapping

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -2,3 +2,4 @@ from .payment_attempt import payment_attempt_received
 from .send_payment_recieved_email import send_payment_recieved_email
 from .check_dividends import check_dividends
 from .donation_handler import donation_handler
+from .success_handler import success_handler
